ad43cddfa98ae76e2f28f06caece58b7866f1f36
Eric Melin <89410095+MelinHead225@users.noreply.github.com>
2022-04-18 11:38:44 -0600

    Update YouthAccount.swift

--- lw2/Banking/Sources/Banking/YouthAccount.swift
+++ lw2/Banking/Sources/Banking/YouthAccount.swift
@@ -19,8 +19,9 @@ class YouthAccount: Account SubAccountProtocol {
        self.bal = bal
     }
     
+    //Youth account has set interest rate at 0.049%.
     override func accrue(rate: Double) {
-        rate = 0.049;
+        rate = 0.049;   //rate is overriden in due to the design of Bank.swift
         self.interest = self.interest + (self.bal * rate);
         self.bal = self.bal + (self.bal * rate);
     }
